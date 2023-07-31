907de09e7616f1aad466a175eb8ac3ed77c78387
Muhammad Ammad Sheikh <139677523+AmmadLala@users.noreply.github.com>
2023-07-31 14:51:54 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+const auth = 'Quaid-e-Azam Muhammad Ali Jinnah';
+const qoute = 'Work,Work and Work and we are bound to success';
+const msg = `${auth} once said,"${qoute}"`;
+console.log(msg);
